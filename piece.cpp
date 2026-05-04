d29a0eaee1a777a33f7bfc4a4dd4a4da3a0865ca
MuhammadAli-Javed <m.alijaved717@gmail.com>
2026-05-04 21:21:09 +0500

    Added Base Class Piece

---
+++ piece.cpp
@@ -0,0 +1,74 @@
+#include "Piece.h"
+
+
+//====== Positon Class implementation ======
+
+//Constructor for initiailization of row and column with given values
+//used when placing or calculating moves
+Position::Position(int r, int c) {
+    row = r;
+    col = c;
+}
+
+//Copy Constructor (Shallow Copy since simple structure)
+Position::Position(const Position &other) {
+    row = other.row;
+    col = other.col;
+}
+
+
+
+//======== PIECE CLASS IMPLEMENTATION ========
+
+//Default Constructor, used when no values provided
+Piece::Piece() {
+    pieceColor = WHITE;
+    currentPos = Position(0, 0);
+    hasMoved = false;
+}
+
+//Parameterized Constructor (used when placing on board during initialization)
+Piece::Piece(Color color, Position pos) {
+    pieceColor = color;
+    currentPos = pos;
+    hasMoved = false;
+}
+
+//Copy Constructor (Shallow Copy)
+Piece::Piece(const Piece &other) {
+    pieceColor = other.pieceColor;
+    currentPos = other.currentPos;
+    hasMoved = other.hasMoved;
+}
+
+//VIRTUAL Destructor, so derived class destructors run properly
+Piece::~Piece() {
+//No dynamic memory,so nothing to free
+}
+
+
+//GETTER FUNCTIONS
+
+Color Piece::getColor() {
+    return pieceColor;
+}
+
+Position Piece::getPosition() {
+    return currentPos;
+}
+
+bool Piece::getHasMoved() {
+    return hasMoved;
+}
+
+
+
+//SETTER FUNCTIONS
+
+void Piece::setPosition(Position pos) {
+    currentPos = pos;
+}
+
+void Piece::setHasMoved(bool moved) {
+    hasMoved = moved;
+}
---
+++ piece.h
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+//Enum for color of the Piece
+enum Color {
+    WHITE, //0
+    BLACK //1
+};
+
+//Represents a position on the board
+//Row and column values range from 0 to 7
+struct Position {
+    int row;
+    int col;
+
+    //Constructor (both default and parameterized)
+    Position(int r =0, int c =0) {}
+
+    //Copy Constructor (Shallopw copy)
+    Position(const Position &other) {} //Copies values of row and col
+
+};
+
+
+//Forward declaration
+//Used because Piece needs Board but Board includes Piece
+class Board;
+
+
+//=============== ABSTRACT BASE CLASS ===============
+//Cannot be instantiated //Cannot directly create objects directly
+//This class will be inherited by ALL chess pieces
+class Piece{
+protected:
+    Color pieceColor; //stores color of the piece (white or black)
+    Position currentPos; //current location on the board
+    bool hasMoved; //used for pawn first move and then castling later
+
+public:
+
+    //Default Constructor
+    //Useful when object is created without parameters
+    Piece() {}
+
+    //Parameterized Constructor (when creating a piece with Specific values)
+    Piece(Color color, Position pos) {}
+
+    //Copy Constructor (Shallow Copy)
+    //Copies values directly (safe here because no dynamic memory inside Piece)
+    Piece(const Piece &other) {}
+
+
+    //Virtual destructor
+    virtual ~Piece() {} //Both Piece and derived class's destructor runs
+
+
+    //PURE VIRTUAL FUNCTION (Polymorphism)
+    //As each derived class MUST implement ITS OWN movement logic
+    // moves[] is array to store the possible moves
+    //moveCount is the number of valid moves found
+    virtual void getValidMoves(Board &board, Position moves[], int &moveCount) = 0;
+
+
+    //Returns the symbol used in printing board for console display
+    virtual char getSymbol() = 0; //(Polymorphism as each piece has its own symbol/character)
+
+
+    //GETTER FUNCTIONS
+    Color getColor() {
+        return pieceColor;
+    }
+
+    Position getPosition() {
+        return currentPos;
+    }
+
+    bool getHasMoved() {
+        return hasMoved;
+    }
+
+    //SETTER FUNCTIONS
+    void setPosition(Position pos) {
+        currentPos = pos;
+    }
+
+    void setHasMoved(bool moved) {
+        hasMoved = moved;
+    }
+};
