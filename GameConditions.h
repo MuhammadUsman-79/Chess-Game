735369077642f9affb9dc1e0951cfbc6ad3e8a05
M.Usman <mu8478229@gmail.com>
2026-05-05 23:53:26 +0500

    Added GameCondition.h and main.cpp

---
+++ GameConditions.h
@@ -0,0 +1,15 @@
+/*#include "board.h"
+
+class GameConditions {
+private:
+    Board* board;
+
+public:
+    GameConditions(Board* b);
+
+    bool is_in_check(bool white);
+    bool is_checkmate(bool white);
+    bool is_stalemate(bool white);  
+    bool is_draw(bool white);
+};
+*/
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<piece.h>
+#include<pawn.h>
+
+using namespace std;
+
+int main(){
+
+
+
+
+
+    return 0;
+}
